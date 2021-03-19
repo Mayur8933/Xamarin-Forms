6a7d32a093d6cffd8ff66976e830f63a6aa675bb
Mayur8933 <mp326395@gmail.com>
2021-03-19 17:17:19 +0530

    Adding new project colorPicker

---
+++ ColorPicker/ColorPicker/LoginPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ColorPicker
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LoginPage : ContentPage
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+
+        private void myButton_Clicked(object sender, EventArgs e)
+        {
+            Application.Current.Properties["UID"] = MyEntry.Text;
+            Navigation.PushAsync(new MainPage());
+        }
+    }
+}
---
+++ ColorPicker/ColorPicker/MainPage.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace ColorPicker
+{
+    public partial class MainPage : ContentPage
+    {
+        public string Uname;
+        public MainPage()
+        {
+            InitializeComponent();
+            Uname = Application.Current.Properties["UID"].ToString();
+        }
+
+        private void OrangeButton_Clicked(object sender, EventArgs e)
+        {
+            TextToSpeech.SpeakAsync($"{Uname},This is Orange Color");
+        }
+        
+        private void RedButton_Clicked(object sender, EventArgs e)
+        {
+            TextToSpeech.SpeakAsync($"{Uname},This is Red Color");
+        }
+
+        private void GreenButton_Clicked(object sender, EventArgs e)
+        {
+            TextToSpeech.SpeakAsync($"{Uname},This is Green Color");
+        }
+
+        private void BlackButton_Clicked(object sender, EventArgs e)
+        {
+            TextToSpeech.SpeakAsync($"{Uname},This is Black Color");
+        }
+
+        private void PinkButton_Clicked(object sender, EventArgs e)
+        {
+            TextToSpeech.SpeakAsync($"{Uname},This is Pink Color");
+        }
+
+        private void YellowButton_Clicked(object sender, EventArgs e)
+        {
+            TextToSpeech.SpeakAsync($"{Uname},This is Yellow Color");
+        }
+
+        private void BlueButton_Clicked(object sender, EventArgs e)
+        {
+            TextToSpeech.SpeakAsync($"{Uname},This is Blue Color");
+        }
+
+
+        private void ChocolateButton_Clicked(object sender, EventArgs e)
+        {
+            TextToSpeech.SpeakAsync($"{Uname},This is Chocolate Color");
+        }
+    }
+}
